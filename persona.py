e3f66565a0f05b817d4f9e9b54a40b4439eaaefb
layard <114928369+layard32@users.noreply.github.com>
2026-01-28 15:28:13 +0100

    fix: id for persona

--- persona.py
+++ persona.py
@@ -1,12 +1,13 @@
 # definisco la classe persona in base ai requisiti
 class Persona:
-    def __init__(self, nome: str, cognome: str, indirizzo: str, telefono: str, eta: int):
+    def __init__(self, nome: str, cognome: str, indirizzo: str, telefono: str, eta: int, id: int = None):
         self.nome = nome
         self.cognome = cognome
         self.indirizzo= indirizzo
         self.telefono = telefono
         self.eta = eta
+        self.id = id
     
     # per debugging
     def __print__(self):
-        print(f"Nome: {self.nome}, Cognome: {self.cognome}, Indirizzo: {self.indirizzo}, Telefono: {self.telefono}, Età: {self.eta}")
+        print(f"Nome: {self.nome}, Cognome: {self.cognome}, ID: {self.id}")
